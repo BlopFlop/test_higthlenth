cbbcfed876d50518fcc7fe0d68105cd82437d679
BlopFlop <arturungb@gmail.com>
2024-12-02 18:56:31 +0300

    All logging config

--- task_tracker/constants.py
+++ task_tracker/constants.py
@@ -3,7 +3,8 @@ from typing import Final
 
 BASE_DIR: Final[Path] = Path(__file__).resolve().parent.parent
 JSON_FILE: Final[Path] = BASE_DIR / "task_db.json"
-JOG_DIR: Final[Path] = BASE_DIR / "logging"
+LOG_DIR: Final[Path] = BASE_DIR / "logging"
+LOG_FILE_NAME: Final[str] = "task_tracker.log"
 
 # parser constants
 DESCRIPTION_CONSOLE_PROGRAM: Final[str] = (
